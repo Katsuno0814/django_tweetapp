e837c18c7883aacdc4e7cca7074b686061ef6595
Ryo Katsuno <62652429+Katsuno0814@users.noreply.github.com>
2024-03-21 00:00:33 +0900

    tweetsモデル、テーブルの作成

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,13 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=10)  # 文字列フィールドはCharFieldを使用します。max_lengthは必須の引数です。
+    text = models.CharField(max_length=255)  # 長いテキストの場合はTextFieldを使用します。
+    image = models.TextField()  # 画像はImageFieldを使用し、upload_toで保存先を指定します。
+    created_at = models.DateTimeField(auto_now_add=True)  # オブジェクト作成時に現在の日時を自動的にセット
+    updated_at = models.DateTimeField(auto_now=True)  # オブジェクト更新時に現在の日時を自動的にセット
+
+    def __str__(self):
+        return self.content
